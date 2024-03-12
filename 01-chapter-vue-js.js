15c6e58f4bbec96486cc44b22b0a7d9d36efca06
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-03-12 16:32:12 +0530

    Add files via upload

---
+++ 01-chapter-vue-js.js
@@ -0,0 +1,26 @@
+/*
+** Popular JavaScript Frameworks
+    - Angular
+    - React
+    - Vue.js
+
+
+** Vue.js vs. other frameworks
+    - Approchable ප්රවේශ විය හැකි
+    - Versatile බහුකාර්ය
+    - Perfomant කාර්ය සාධනය
+
+
+** Approchable 
+    . Vue.js is beginner friendly while also poerful enough to grow with you and your applications.
+
+** Versatile
+    . Veu.js is flexible enough to be integrated into existing projects or it can be used to buil a new
+      application from the ground page 
+
+** Performant 
+    . Veu.js is lightweight, allowing you to start with the core frame work, pulling in additional
+      features as you need them (e.g. routing, state management, sever-side redering, testing)
+
+        
+*/
